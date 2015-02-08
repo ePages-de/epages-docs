452e3c9151cc2aac019dfdcc89f60bf79a939fec
Christian Hoffmeister <mail@choffmeister.de>
2015-02-08 23:09:44 +0100

    Make API console and RAML raw data hideable

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -3,6 +3,11 @@ module Jekyll
     def chunked_array(array, chunk_size)
       array.each_slice(chunk_size).to_a
     end
+
+    def jsonify_pretty(input)
+      require 'json'
+      JSON.pretty_generate(input)
+    end
   end
 end
 
