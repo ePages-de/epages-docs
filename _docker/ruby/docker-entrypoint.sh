06c3a063b1336992ec64dddae47de34a1d7477a2
Benjamin Nothdurft <b.nothdurft@gmail.com>
2016-03-04 05:13:24 +0100

    Add parallelel build and deployment of images with post step

--- _docker/ruby/docker-entrypoint.sh
+++ _docker/ruby/docker-entrypoint.sh
@@ -6,16 +6,20 @@ set -e
 # Always own possibly mounted docker volumes (files on host).
 chown -R root:root ${EPAGES_DOCS}
 
-# If the first argument is test, build, index or serve 
+# If the first argument is one of the following args
 # then set exec to prepend with rake.
-if [[ "${1}" =~ ^.*(test)|(build)|(index)|(serve).*$ ]]; then
+if [[ "${1}" =~ ^(build)|(index)|(serve)|(test)$ ]]; then
     set rake "${@}"
 fi
 
 # If the first argument is rake
 # then set exec with all given args.
 if [[ "${1}" == "rake" ]]; then
-	bundle install
+    # Make sure new gems of the mounted local repo are added.
+    bundle install
+    # Test the syntax of created the HTML files.
+    bundle exec rake test
+    # Run the desired command.
     set "${@}"
 fi
 
