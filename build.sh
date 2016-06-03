bf808cda22871ef57b1a2ab2de04ec027f747db5
Christian Hoffmeister <mail@choffmeister.de>
2016-06-03 09:57:06 +0200

    Push docker images to official docker hub repo

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 IMAGE_ELASTICSEARCH_BASE="choffmeister/elasticsearch-prefilled"
 IMAGE_ELASTICSEARCH_TEMP="epages-docs-elasticsearch-${RANDOM}${RANDOM}${RANDOM}"
 
-IMAGE_ELASTICSEARCH="docker.epages.com/epages/docs-elasticsearch"
-IMAGE_NGINX="docker.epages.com/epages/docs-nginx"
+IMAGE_ELASTICSEARCH="epages/docs-elasticsearch"
+IMAGE_NGINX="epages/docs-nginx"
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${DIR}"
@@ -17,13 +17,13 @@ bundle exec jekyll build
 
 # build elasticsearch image
 docker run --name "${IMAGE_ELASTICSEARCH_TEMP}" -p 9200:9200 -d "${IMAGE_ELASTICSEARCH_BASE}"
-sleep 10
+sleep 15
 if [ -z "${ELASTICSEARCH}" ]; then
   bundle exec jekyll index
 else
   JEKYLL_SEARCH_HOST="${ELASTICSEARCH}" bundle exec jekyll index
 fi
-sleep 10
+sleep 15
 docker stop "${IMAGE_ELASTICSEARCH_TEMP}"
 docker commit "${IMAGE_ELASTICSEARCH_TEMP}" "${IMAGE_ELASTICSEARCH}:${TAG}"
 docker rm -vf "${IMAGE_ELASTICSEARCH_TEMP}" || true
