f99851395941a7d9e0af75a0c1fd437263b4330f
Gristof <muchrist720@gmail.com>
2018-01-23 16:04:20 +0100

    Center images

--- _plugins/image.rb
+++ _plugins/image.rb
@@ -11,8 +11,8 @@ module Jekyll
     def render(context)
       url = unless @url =~ /^https?:\/\// then context.registers[:site].baseurl + '/assets/images/apps/' + @url else @url end
       width_html = unless @width.nil? then "style='width: #{@width}'" else '' end
-      image_html = "<img src=\"#{url}\" alt=\"Image could not be loaded\" #{width_html if @float.nil?}/>"
-      "<a class=\"imagebox #{@float}\" #{width_html if @float} href=\"#{url}\" data-lightbox=\"imagebox\">#{image_html}</a>"
+      image_html = "<img src='#{url}' alt='#{@url}' #{width_html if @float.nil?}/>"
+      "<a class='imagebox #{@float}' #{width_html if @float} href='#{url}' data-lightbox='imagebox'>#{image_html}</a>"
     end
   end
 end
