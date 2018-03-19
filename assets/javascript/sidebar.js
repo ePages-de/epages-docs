bc825dc3ef5304d09a6beea7c09bf3716e375be8
unabris <u.abrisketa@epages.com>
2018-03-19 08:45:44 +0100

    Add close behavior when clicking same menu entry

--- assets/javascript/sidebar.js
+++ assets/javascript/sidebar.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
   $('li.sitemap-headline').click(function() {
     if ($(window).width() <= 992){
-      $('.sitemap-content').removeClass('sitemap-content--open');
-      $(this).next().addClass('sitemap-content--open');
-      $('.sitemap-headline svg').removeClass('fa-caret-up').addClass('fa-caret-down');
-      $('svg', this).removeClass('fa-caret-down').addClass('fa-caret-up');
+      if ($(this).next().hasClass('sitemap-content--open')) {
+        $(this).next().removeClass('sitemap-content--open');
+      } else {
+        $('.sitemap-content').removeClass('sitemap-content--open');
+        $(this).next().addClass('sitemap-content--open');
+        $('.sitemap-headline svg').removeClass('fa-caret-up').addClass('fa-caret-down');
+        $('svg', this).removeClass('fa-caret-down').addClass('fa-caret-up');
+      }
     }
   })
 });
