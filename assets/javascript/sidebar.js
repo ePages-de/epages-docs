8f2a124c29413d86d15f7ae4c0d1c75366880ae2
gersanco <g.sanemeterio@epages.com>
2019-07-23 11:51:41 +0200

    Sidebar of menu when is responsive

--- assets/javascript/sidebar.js
+++ assets/javascript/sidebar.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   $('li.sitemap-headline').click(function() {
     if ($(window).width() <= 992){
       if ($(this).next().hasClass('sitemap-content--open')) {
-        $(this).next().removeClass('sitemap-content--open');
+        $(this).next().slideUp().removeClass('sitemap-content--open');
       } else {
         $('.sitemap-content').removeClass('sitemap-content--open');
-        $(this).next().addClass('sitemap-content--open');
+        $(this).next().slideDown().addClass('sitemap-content--open');
         $('.sitemap-headline svg').removeClass('fa-caret-up').addClass('fa-caret-down');
         $('svg', this).removeClass('fa-caret-down').addClass('fa-caret-up');
       }
@@ -14,8 +14,6 @@ $(document).ready(function() {
 });
 
 function changeSiteMap(element) {
-  console.log({element})
-  // debugger
   var id = $(element).text().trim().toLowerCase();
   if($('svg', element).first().hasClass('fa-caret-down')) {
     $('svg', element).first().removeClass('fa-caret-down').addClass('fa-caret-up');
