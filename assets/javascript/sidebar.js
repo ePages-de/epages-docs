96e4b39dd717c6485da2097bcb0e562ffe97bb88
gersanco <g.sanemeterio@epages.com>
2019-07-24 11:08:50 +0200

    Change the color when is activate

--- assets/javascript/sidebar.js
+++ assets/javascript/sidebar.js
@@ -11,10 +11,11 @@ $(document).ready(function() {
       }
     }
   })
-  $('li.sitemap-entry-expand').click(function() {
+  $('li.sitemap-entry-title, li.sitemap-entry-expand').click(function() {
+    $('li.sitemap-entry-title').removeClass('active');
     $('li.sitemap-entry-expand.active').removeClass('active');
-    $(this).addClass('active');
-  })
+    $(this).addClass('active')
+  });
 });
 
 function changeSiteMap(element) {
