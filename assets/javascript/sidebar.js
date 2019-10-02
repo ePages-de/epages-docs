be63243099002ec597f1eab9358df58be7d24f33
German <g.sanemeterio@epages.com>
2019-10-02 15:03:30 +0200

    Revert "Beyond docs layout"

--- assets/javascript/sidebar.js
+++ assets/javascript/sidebar.js
@@ -2,29 +2,13 @@ $(document).ready(function() {
   $('li.sitemap-headline').click(function() {
     if ($(window).width() <= 992){
       if ($(this).next().hasClass('sitemap-content--open')) {
-        $(this).next().slideUp().removeClass('sitemap-content--open');
+        $(this).next().removeClass('sitemap-content--open');
       } else {
         $('.sitemap-content').removeClass('sitemap-content--open');
-        $(this).next().slideDown().addClass('sitemap-content--open');
+        $(this).next().addClass('sitemap-content--open');
         $('.sitemap-headline svg').removeClass('fa-caret-up').addClass('fa-caret-down');
         $('svg', this).removeClass('fa-caret-down').addClass('fa-caret-up');
       }
     }
   })
-  $('li.sitemap-entry, li.sitemap-entry-expand').click(function() {
-    $('li.sitemap-entry').removeClass('active');
-    $('li.sitemap-entry-expand.active').removeClass('active');
-    $(this).addClass('active')
-  });
 });
-
-function changeSiteMap(element) {
-  var id = $(element).text().trim().toLowerCase();
-  if($('svg', element).first().hasClass('fa-caret-down')) {
-    $('svg', element).first().removeClass('fa-caret-down').addClass('fa-caret-up');
-    $('#'+id).slideDown().removeClass('sitemap-content-expand--close').addClass('sitemap-content-expand--open');
-  } else {
-    $('svg', element).first().removeClass('fa-caret-up').addClass('fa-caret-down');
-    $('#'+id).slideUp().removeClass('sitemap-content-expand--open').addClass('sitemap-content-expand--close');
-  }
-}
--- beyond-docs/index.md
+++
@@ -1,6 +0,0 @@
----
-layout: beyond-docs
-key: apps-overview
-title: Overview
-header: Beyond REST API documentation
----
